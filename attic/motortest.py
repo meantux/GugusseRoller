b554593617d2caeb8872aad2f2571e334e1247f1
Denis-Carl Robidoux <deniscarl@gmail.com>
2018-04-19 01:05:47 -0400

    New board with trynamics motor drivers... just notes before coding for real

---
+++ attic/motortest.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+
+GPIO.setmode(GPIO.BCM) 
+
+#ena=18
+#inv=14
+#stp=15
+
+#ena=25
+#inv=23
+#stp=24
+
+ena=21
+inv=8
+stp=7
+
+
+GPIO.setup(ena,GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(inv,GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(stp,GPIO.OUT, initial=GPIO.HIGH)
+
+while True:
+    count=10000
+    GPIO.output(inv, GPIO.HIGH)
+    while count > 0:
+        GPIO.output(stp, GPIO.LOW)
+        sleep (0.0001)
+        GPIO.output(stp,GPIO.HIGH)
+        sleep(0.0001)
+        count-= 1
+
+    count=10000
+    GPIO.output(inv, GPIO.LOW)
+    while count > 0:
+        GPIO.output(stp, GPIO.LOW)
+        sleep (0.0001)
+        GPIO.output(stp,GPIO.HIGH)
+        sleep(0.0001)
+        count-= 1
+    
+
