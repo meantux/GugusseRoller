e15b4b2c9c38ab0337345c7f173513e639d1ea58
Denis-Carl Robidoux <deniscarl@gmail.com>
2019-02-16 12:50:37 -0500

    Merge to sync small details before work on cameraAdjust.py

--- attic/preview.py
+++ attic/preview.py
@@ -7,10 +7,19 @@ from fractions import Fraction
 c=PiCamera()
 #c.resolution=(1024,768)
 #c.resolution=(1440,1080)
-c.shutter_speed=int(16600*2.0)
 #c.awb_mode='off'
 #c.awb_gains=(Fraction(229,256),Fraction(763,256))
+#c.iso=100
 c.start_preview()
+#sleep(1)
+#c.exposure_mode="off"
+#c.iso=25
+#c.shutter_speed=8000
+#sleep(2)
+#c.capture("test.jpg")
 while [1] :
-    sleep(1)
+    #c.led=False
+    sleep(3)
+    #c.led=True
+    sleep(3)
 #c.capture("test.jpg")
