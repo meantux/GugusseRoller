2e3e104e40e42c49f8bd552587d99255dc51833c
Denis-Carl Robidoux <deniscarl@gmail.com>
2021-11-08 23:49:51 -0500

    WIP GUI implementation (backup push)

--- GCamera.py
+++ GCamera.py
@@ -16,9 +16,6 @@ class GCamera(PiCamera):
             self.captureModes=json.load(h)
         self.resolution=self.MAX_RESOLUTION
         #self.start_preview(fullscreen=False,resolution=(1024,768),window=(256,0,1024,768),vflip=False,hflip=False)
-        self.gcCamModes=[ "off", "auto", "night", "nightpreview", "backlight", "spotlight", "sports", "snow", "beach", "verylong", "fixedfps", "antishake", "fireworks"]
-        self.gcAwbModes=[ "off", "auto", "sunlight", "cloudy", "shade", "tungsten", "fluorescent", "incandescent", "flash", "horizon"]
-        self.gcMeterModes=[ "average", "spot", "backlit", "matrix" ]
         self.gcZooms=[(0.0,0.0,1.0,1.0), (0.0,0.0,0.333,0.333), (0.333,0.0,0.333,0.333), (0.667,0.0,0.333,0.333), (0.0,0.333,0.333,0.333), (0.333,0.333,0.333,0.333), (0.667,0.333,0.333,0.333), (0.0,0.667,0.333,0.333), (0.333,0.667,0.333,0.333), (0.667,0.667,0.333,0.333)]
         # As recommended by the documentation on the raspberri pi camera
         # we have to leave it in automatic a few seconds before setting
--- Interface.py
+++ Interface.py
@@ -46,7 +46,7 @@ with open("filmFormats.json","rt") as h:
 filmFormatsList=[]
 for item in filmFormatsDetails:
     filmFormatsList.append(str(item))
-
+    
 captureMode=StringVar(root)
 captureMode.set(settings["captureMode"])
 
@@ -56,15 +56,31 @@ filmFormat.set(settings["filmFormat"])
 exposureMode=StringVar(root)
 exposureMode.set(settings["exposure_mode"])
 
+meterMode=StringVar(root)
+meterMode.set(settings["meter_mode"])
+cam.meter_mode=settings["meter_mode"]
+
 awbMode=StringVar(root)
 awbMode.set(settings["awb_mode"])
+cam.awb_mode=settings["awb_mode"]
+
 
 imageEffect=StringVar(root)
 imageEffect.set(settings["image_effect"])
+cam.image_effect=settings["image_effect"]
+
+rawFormat=StringVar(root)
+rawFormat.set(settings["raw_format"])
+cam.raw_format=settings["raw_format"]
 
 def handleExposureChange(event):
     settings["shutter_speed"]=int(event)
     cam.shutter_speed=int(event)
+
+def handleMeterModeChange(event):
+    val=str(event)
+    settings["meter_mode"]=val
+    cam.meter_mode=val
 
 def handleWbGain1(event):
     settings["awb_gains"][0]=float(event)
@@ -82,15 +98,20 @@ def  handleImageEffectChange(event):
 def handleAwbModeChange(event):
     val=str(event)
     settings["awb_mode"]=val
-    if val == "off":
-        wbGain1.configure(state="normal")
-        wbGain2.configure(state="normal")
-            
-        
-    else:
-        wbGain1.configure(state="disabled")
-        wbGain2.configure(state="disabled")
+    #if val == "off":
+    #    wbGain1.configure(state="normal")
+    #    wbGain2.configure(state="normal")
+    #        
+    #    
+    #else:
+    #    wbGain1.configure(state="disabled")
+    #    wbGain2.configure(state="disabled")
     cam.awb_mode=val
+
+def handleRawFormatChange(event):
+    val=str(event)
+    settings["raw_format"]=val
+    cam.raw_format=val
     
 
 def handleExposureModeChange(event):
@@ -101,13 +122,21 @@ def handleExposureModeChange(event):
         if handleExposureModeChange.ExposureWasNotOff:
             exposition.set(20000)
             cam.shutter_speed=20000
-            handleExposureModeChange.ExposureWasNotOff=False
-    else:
-        exposition.configure(state="disabled",fg="gray")
-        handleExposureModeChange.ExposureWasNotOff=True
+            #handleExposureModeChange.ExposureWasNotOff=False
+    #else:
+        #exposition.configure(state="disabled",fg="gray")
+        #handleExposureModeChange.ExposureWasNotOff=True
     cam.exposure_mode=val
+
+def handleIsoChange(event):
+    val=int(event)
+    settings["iso"]=val
+    cam.iso=val
+    analog_gain.configure(text="Gain: {}".format(float(cam.analog_gain)))
+
 handleExposureModeChange.ExposureWasNotOff=False
-        
+
+
 
 miniFrame=Frame(topFrame, highlightbackground="black", highlightthickness=1)
 imageEffect=StringVar(root)
@@ -115,12 +144,16 @@ imageEffect.set(settings["image_effect"])
 
 
 miniFrame.pack(side="top",fill="x")
-exposition=Scale(miniFrame,from_=1,to=32767,length=scr_w-widget_w,width=8,orient=HORIZONTAL,command=handleExposureChange)
+exposition=Scale(miniFrame,from_=1,to=32700,resolution=100,length=scr_w/2-widget_w,width=8,orient=HORIZONTAL,command=handleExposureChange)
 exposition.set(settings["shutter_speed"])
 exposition.pack(side="right")
-miniFrame=Frame(miniFrame, highlightbackground="black", highlightthickness=1)
-miniFrame.pack(side="right",ipadx=widget_w)
-Label(miniFrame,text="Exposure:").pack(side="right")
+Label(miniFrame,text="Exposure:",width=widget_wchars,anchor="e").pack(side="right")
+
+iso=Scale(miniFrame,from_=100,to=800,resolution=100,length=scr_w/2-widget_w,width=8,orient=HORIZONTAL,command=handleIsoChange)
+iso.set(settings["iso"])
+iso.pack(side="right")
+Label(miniFrame,text="Iso:").pack(side="right")
+
 
 miniFrame=Frame(topFrame, highlightbackground="black", highlightthickness=1)
 miniFrame.pack(side="top",fill="x")
@@ -164,7 +197,7 @@ lbl.pack(side="right")
 
 miniFrame=Frame(leftFrame, highlightbackground="black", highlightthickness=1)
 miniFrame.pack(side="top",fill="x")
-exposureModeSelector=OptionMenu(miniFrame,exposureMode,*cam.gcCamModes,command=handleExposureModeChange)
+exposureModeSelector=OptionMenu(miniFrame,exposureMode,*cam.EXPOSURE_MODES.keys(),command=handleExposureModeChange)
 exposureModeSelector.config(width=widget_wchars)
 exposureModeSelector.pack(side="right")
 lbl=Label(miniFrame,text="Exposure Mode:",width=widget_wchars,anchor="e")
@@ -172,7 +205,18 @@ lbl.pack(side="right")
 
 miniFrame=Frame(leftFrame, highlightbackground="black", highlightthickness=1)
 miniFrame.pack(side="top",fill="x")
-awbModeSelector=OptionMenu(miniFrame,awbMode,*cam.gcAwbModes,command=handleAwbModeChange)
+meterModeSelector=OptionMenu(miniFrame,meterMode,*cam.METER_MODES.keys(),command=handleMeterModeChange)
+meterModeSelector.config(width=widget_wchars)
+meterModeSelector.pack(side="right")
+lbl=Label(miniFrame,text="Meter Mode:",width=widget_wchars,anchor="e")
+lbl.pack(side="right")
+
+
+
+
+miniFrame=Frame(leftFrame, highlightbackground="black", highlightthickness=1)
+miniFrame.pack(side="top",fill="x")
+awbModeSelector=OptionMenu(miniFrame,awbMode,*cam.AWB_MODES.keys(),command=handleAwbModeChange)
 awbModeSelector.config(width=widget_wchars)
 awbModeSelector.pack(side="right")
 lbl=Label(miniFrame,text="AWB Mode:",width=widget_wchars,anchor="e")
@@ -186,13 +230,30 @@ imageEffectSelector.pack(side="right")
 lbl=Label(miniFrame,text="ImageEffect:",width=widget_wchars,anchor="e")
 lbl.pack(side="right")
 
-if str(awbModeSelector) != "off":
-    wbGain1.configure(state="disabled")
-    wbGain2.configure(state="disabled")
+miniFrame=Frame(leftFrame, highlightbackground="black", highlightthickness=1)
+miniFrame.pack(side="top",fill="x")
+rawFormatSelector=OptionMenu(miniFrame,rawFormat,*cam.RAW_FORMATS,command=handleRawFormatChange)
+rawFormatSelector.config(width=widget_wchars)
+rawFormatSelector.pack(side="right")
+lbl=Label(miniFrame,text="Raw Format:",width=widget_wchars,anchor="e")
+lbl.pack(side="right")
 
-if exposureMode.get() != "off":
-    print("mode is {}".format(exposureMode.get()))
-    exposition.configure(state="disabled")
+
+
+
+
+miniFrame=Frame(leftFrame, highlightbackground="black", highlightthickness=1)
+miniFrame.pack(side="top",fill="x")
+analog_gain=Label(miniFrame,text="analog_gain=?",width=widget_wchars,anchor="e")
+analog_gain.pack(side="right")
+
+#if str(awbModeSelector) != "off":
+#    wbGain1.configure(state="disabled")
+#    wbGain2.configure(state="disabled")
+
+#if exposureMode.get() != "off":
+#    print("mode is {}".format(exposureMode.get()))
+#    exposition.configure(state="disabled")
               
 
 root.mainloop()
