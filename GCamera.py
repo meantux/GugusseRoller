3863851fe086a22cca350182d0b15e51aa7deb4d
Denis-Carl Robidoux <denis-carl.robidoux@buybackbooth.com>
2023-06-03 23:06:54 -0400

    Added buffers dumping to avoid grabbing past pictures

--- GCamera.py
+++ GCamera.py
@@ -9,6 +9,8 @@ from libcamera import Transform
 
 
 
+
+
 defaultValues={
     "fps":10
 }
@@ -16,17 +18,17 @@ def setMissingToDefault(settings):
     for key in defaultValues:
         if key not in settings:
             settings[key]=defaultValues[key]
-
+            
 class GCamera(Picamera2):
     def __init__(self, win):
         Picamera2.__init__(self)
         self.win=win
         setMissingToDefault(win.settings)
+        self.fps=self.win.settings["fps"]
         self.framecount=0
         with open("captureModes.json","r") as h:
             self.captureModes=json.load(h)
 
-        self.fps=win.settings["fps"]
         vflip=False
         hflip=False
         if "hflip" in win.settings:
@@ -48,20 +50,32 @@ class GCamera(Picamera2):
             
     def setFileIndex(self, newIndex):
         self.framecount=newIndex
+
+
+    def skipBuffers(self, count, which):
+        while count > 0:
+            start_time = time()
+            buffers, metadata = self.capture_buffers([which])
+            elapsed_time = time() - start_time
+            sleep_time = max(0, (1.0 / self.fps) - elapsed_time)
+            sleep(sleep_time)
+            count -= 1
         
     def captureCycle(self):
         captureMode=self.win.captureMode.currentText()
         if captureMode == "singleJpg":
+            self.skipBuffers(3, "main")
             fn="/dev/shm/{:05d}.jpg".format(self.framecount)
             fnComplete="/dev/shm/complete/{:05d}.jpg".format(self.framecount)
+            
             buffers,metadata=self.capture_buffers(["main"])
             orig=self.helpers.make_image(buffers[0], self.config["main"]).convert('RGB')
             orig.save(fn)
             os.rename(fn,fnComplete)
 
         elif captureMode == "bracketing":
+            self.skipBuffers(3, "main")
             shutter=self.win.settings["ExposureMicroseconds"]
-            fps=self.win.settings["fps"]            
             fn="/dev/shm/{:05d}_m.jpg".format(self.framecount)
             fnComplete="/dev/shm/complete/{:05d}_m.jpg".format(self.framecount)
             buffers,metadata=self.capture_buffers(["main"])
@@ -69,7 +83,7 @@ class GCamera(Picamera2):
             orig.save(fn)
             self.shutter_speed=int(shutter/2)
             os.rename(fn, fnComplete)
-            sleep(3.0/float(fps))
+            self.skipBuffers(3, "main")
             fn="/dev/shm/{:05d}_l.jpg".format(self.framecount)
             fnComplete="/dev/shm/complete/{:05d}_l.jpg".format(self.framecount)
             buffers,metadata=self.capture_buffers(["main"])
@@ -77,7 +91,7 @@ class GCamera(Picamera2):
             orig.save(fn)
             self.shutter_speed=int(shutter*2)
             os.rename(fn, fnComplete)
-            sleep(3.0/float(fps))
+            self.skipBuffers(3, "main")
             fn="/dev/shm/{:05d}_h.jpg".format(self.framecount)
             fnComplete="/dev/shm/complete/{:05d}_h.jpg".format(self.framecount)
             buffers,metadata=self.capture_buffers(["main"])
@@ -87,6 +101,7 @@ class GCamera(Picamera2):
             self.shutter_speed=int(shutter)
             
         elif captureMode == "DNG":
+            self.skipBuffers(3, "raw")
             fn=f"/dev/shm/{self.framecount:05d}.dng"
             fnComplete=f"/dev/shm/complete/{self.framecount:05d}.dng"
             buffers,metadata=self.capture_buffers(["raw"])
